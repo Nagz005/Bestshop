1329a6d54d1d09ee497e09f64e93b3df1e2ddf09
Nagz005 <131228907+Nagz005@users.noreply.github.com>
2023-12-11 15:08:46 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName: 'Foldable mini Drone',
+        productNumber: '85632',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'FLarevnder KF!100',
+        productNumber: '56748',
+        paymentStatus: 'Refuded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko f11 pro',
+        productNumber: '8474',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'iphone 15',
+        productNumber: '34585',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'settle cock',
+        productNumber: '85632',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+
+]
